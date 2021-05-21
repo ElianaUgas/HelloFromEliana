b09fc6b40bfbe989fd861814e3207675787f49bd
unknown <u398371819@byui.edu>
2021-05-20 21:09:53 -0400

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,6 @@
+//Week04 prove. Creating a single java program to put into GitHub for practice
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, my name is Eliana");
+    }
+}
